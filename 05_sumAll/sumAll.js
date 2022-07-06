19120e98b9f3eb49e66dd87dbba53edf01bee268
maxthor500 <iraubuntu@gmail.com>
2022-07-06 17:32:42 +0100

    Add a solution and test sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (((typeof num1 !== "number")) || 
+        (typeof num2 !== "number")) return "ERROR";
 
+    if (num1 < 0 || num2 < 0) return "ERROR";
+
+    let numbers = [num1, num2].sort()
+
+    let sum  = 0;
+    for (let i = numbers[0]; i <= numbers[1]; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line

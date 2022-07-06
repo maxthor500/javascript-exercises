68a7ec3a94ebc325cb2b9fa3b9bf826b422f54f1
maxthor500 <iraubuntu@gmail.com>
2022-07-06 16:48:57 +0100

    Add a solution to removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    let result = arr.filter(element => {
+        if (![...args].includes(element)) return element
+    })
+    return result;
 };
 
 // Do not edit below this line

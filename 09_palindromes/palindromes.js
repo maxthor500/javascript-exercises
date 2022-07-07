7c101c5aafdad2c79b4c8287249eb1f4528e0fab
maxthor500 <iraubuntu@gmail.com>
2022-07-07 09:17:41 +0100

    Add a solution to palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let punctuationless = word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    let spaceless = punctuationless.replace(/\s*/g,"").toLowerCase();
 
+    let reverse = ""
+    for (let i = spaceless.length - 1; i >= 0; i--) {
+        reverse += spaceless[i];
+    }
+
+    if (spaceless === reverse) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line

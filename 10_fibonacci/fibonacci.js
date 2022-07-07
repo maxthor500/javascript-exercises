ea59eeaf8a34367d41ba7f39be6627df781ea3c6
maxthor500 <iraubuntu@gmail.com>
2022-07-07 09:56:35 +0100

    Add a solution to fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let sequence = [0];
 
+    if (num < 0) return "OOPS";
+
+    for (let i = 1; i <= parseInt(num); i++) {
+        if (i > 1) {
+            sequence.push(sequence[i-1] + sequence[i-2]);
+        } else {
+            sequence.push(1);
+        }
+    }
+
+    return sequence[sequence.length -1]
 };
 
 // Do not edit below this line
